3070491cd1cda97e000bc02d4591387a5d1d218c
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-05-31 09:33:54 -0300

    projeto

---
+++ back-end/pel/src/main/java/com/projetoensinolivre/pel/PelApplication.java
@@ -0,0 +1,13 @@
+package com.projetoensinolivre.pel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelApplication.class, args);
+	}
+
+}
---
+++ back-end/pel/src/test/java/com/projetoensinolivre/pel/PelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoensinolivre.pel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
