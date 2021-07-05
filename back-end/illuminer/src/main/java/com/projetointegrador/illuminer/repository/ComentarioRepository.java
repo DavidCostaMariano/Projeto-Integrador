5d8d59fa5dd84810dd639dba39e02b49cfc93279
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-07-05 17:44:07 -0300

    comentarios paginado

--- back-end/illuminer/src/main/java/com/projetointegrador/illuminer/repository/ComentarioRepository.java
+++ back-end/illuminer/src/main/java/com/projetointegrador/illuminer/repository/ComentarioRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projetointegrador.illuminer.model.Comentario;
@@ -14,6 +15,12 @@ import com.projetointegrador.illuminer.model.Usuario;
 
 @Repository
 public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
+	
+	@Query(value = "SELECT * FROM tb_comentario p WHERE p.postagem_id = ?1", nativeQuery = true)
+	Page<Comentario> paginarComentariosPorPostagem(Long idPostagem, Pageable pageable);
+		
+	@Query(value = "SELECT * FROM tb_comentario p WHERE p.postagem_id = ?1", nativeQuery = true)
+	List<Comentario> listarComentariosPorPostagem(Long idPostagem);
 	
 	Page<Comentario> findByUsuario(Usuario usuario, Pageable pageable);
 
